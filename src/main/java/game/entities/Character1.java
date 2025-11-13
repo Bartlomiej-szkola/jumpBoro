dea74bf0e7fdd0352c18f9c7d39ca1beef8309ee
KrystianT <krystian.tomczyk@tm1.edu.pl>
2025-11-13 23:55:20 +0100

    13.11

--- src/main/java/game/entities/Character1.java
+++ src/main/java/game/entities/Character1.java
@@ -4,6 +4,6 @@ public class Character1 extends Player {
 
     public Character1() {
         // Wywołanie konstruktora Player z predefiniowanymi parametrami
-        super("/character1.png", "/character1jumping.png", "/character1beforejump.png", 2.5 , 30 , 1.0);
+        super("/character1.png", "/character1jumping.png", "/character1beforejump.png", 3.5 , 30 , 1.0);
     }
 }
